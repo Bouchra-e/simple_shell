7db32ac3626129b800dbf503fb15875ae82f6fa6
Bouchra-e <125470165+Bouchra-e@users.noreply.github.com>
2023-05-27 15:34:03 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-directory="$1"
-if [ -z "$directory" ]; then
-  directory="."
+directory="${1:-.}"
+
+if [ ! -d "$directory" ]; then
+  echo "the repository does not exist"
+  exit 1
 fi
 
-/bin/ls "$directory"
+ls -al "$directory"
